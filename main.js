8d0c2f1d99835cecf180003149656a42cd13bd1c
L U T H E R <dafourhafid6@example.com>
2023-05-18 18:55:41 +0100

    added the check box input to switch between yearly and monthly, not functional yet

---
+++ main.js
@@ -0,0 +1,6 @@
+const ckkk = document.querySelector("body");
+const stepOne = document.querySelector(".step-one");
+
+ckkk.addEventListener("click", () => {
+  stepOne.classList.add("active-form");
+});
